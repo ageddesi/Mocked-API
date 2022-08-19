3e9ed4b0e87ce129e8abfeccd01bb9b67aef53e9
Aaron <aaron.rackley9@gmail.com>
2022-08-19 17:16:21 +0100

    Added cors and logging

--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 // Load Mock Modules
 require('./modules/names/api/route')(app); // Names
+
+app.use(cors()); // enabling CORS for all requests
+app.use(morgan('combined'));  // adding morgan to log HTTP requests
 
 app.listen(port, () => {
   console.log(`Mock API is running on port ${port}.`);
