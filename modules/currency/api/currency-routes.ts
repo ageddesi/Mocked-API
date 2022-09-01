6bd303402a7460830cf0cdcf75678f3efc188247
Aaron <aaron.rackley9@gmail.com>
2022-09-01 17:49:20 +0100

    feature: Added digital currency addresses endpoints

--- modules/currency/api/currency-routes.ts
+++ modules/currency/api/currency-routes.ts
@@ -2,6 +2,9 @@ import { Request, Response } from 'express';
 import * as core from 'express-serve-static-core';
 import currencySnapshot from '../data/currency-snapshot';
 import GBP_USD_TICKER_DATA from '../data/gbp-usd-ticker-data';
+import { getQtyFromRequest } from '../../../utils/route-utils';
+import getDigitalCurrencyAddress from '../utils/getDigitalCurrencyAddress';
+import DigitalCoinEnum from '../consts/DigitalCoinEnum';
 
 module.exports = function(app: core.Express){
 
@@ -15,5 +18,25 @@ module.exports = function(app: core.Express){
         res.json(GBP_USD_TICKER_DATA);
     })
 
+    // Returns a set of bitcoin digital addresses
+    app.get("/currencies/digital-coins/bitcoin/random-address/:qty?", (req: Request, res: Response) => {
+        const qty = getQtyFromRequest(req); 
+        const addresses = getDigitalCurrencyAddress(qty, DigitalCoinEnum.Bitcoin);
+        res.json(addresses);
+    })
+
+    // Returns a set of ethereum digital addresses
+    app.get("/currencies/digital-coins/ethereum/random-address/:qty?", (req: Request, res: Response) => {
+        const qty = getQtyFromRequest(req);
+        const addresses = getDigitalCurrencyAddress(qty, DigitalCoinEnum.Ethereum);
+        res.json(addresses);
+    })
+
+    // Returns a set of litecoin digital addresses
+    app.get("/currencies/digital-coins/litecoin/random-address/:qty?", (req: Request, res: Response) => {
+        const qty = getQtyFromRequest(req);
+        const addresses = getDigitalCurrencyAddress(qty, DigitalCoinEnum.Litecoin);
+        res.json(addresses);
+    })
 
 }
