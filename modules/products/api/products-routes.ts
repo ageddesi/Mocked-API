3113a2d7c2ce34be1d916568db692138cbbe3a3c
Trigstur <trigstur@gmail.com>
2022-10-01 22:09:01 +0200

    feat(#48): add product(s) endpoints

--- modules/products/api/products-routes.ts
+++ modules/products/api/products-routes.ts
@@ -1,8 +1,34 @@
 import { Request, Response } from 'express';
 import * as core from 'express-serve-static-core';
 import productReviews from '../data/product-reviews';
+import { getQtyFromRequest } from '../../../utils/route-utils';
+import getProducts from '../utils/getProducts';
 
 module.exports = function(app : core.Express){
+
+    // Returns a random product
+    app.get("/product/", (req: Request, res: Response) => {
+        res.json(getProducts(1))
+    })
+
+    // Returns a random product under a specified department
+    app.get("/product/:department", (req: Request, res: Response) => {
+        const department = req.params.department ? req.params.department.toString() : "General";
+        res.json(getProducts(1, department))
+    })
+
+    // Returns a random list of products
+    app.get("/products/:qty?", (req: Request, res: Response) => {
+        const qty = getQtyFromRequest(req);
+        res.json(getProducts(qty))
+    })
+
+    // Returns a random list of products under a specified department
+    app.get("/products/:qty/:department", (req: Request, res: Response) => {
+        const qty = getQtyFromRequest(req);
+        const department = req.params.department ? req.params.department.toString() : "General";
+        res.json(getProducts(qty, department))
+    })
 
     // Returns a set of reviews
     app.get("/products/reviews", (req: Request, res: Response) => {
