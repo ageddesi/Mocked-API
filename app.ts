b50ccb2f3d0d4dfcb60f585adfb992b171373f2d
tzii <tuanvundvn@gmail.com>
2022-10-02 09:41:10 +0700

    chore: settings swagger jsdoc

--- app.ts
+++ app.ts
@@ -1,5 +1,7 @@
 require('dotenv').config();
-const express = require('express');
+import express, { Request, Response } from 'express';
+import { swaggerSpec } from './utils/swagger';
+
 const morgan = require('morgan');
 const cors = require('cors');
 
@@ -7,8 +9,8 @@ const app = express();
 const port = 3000;
 
 // Load Mock Modules
-require('./modules/animal/api/animal-routes')(app) // Animals
-require('./modules/chat/api/chat-routes')(app) // Chat
+require('./modules/animal/api/animal-routes')(app); // Animals
+require('./modules/chat/api/chat-routes')(app); // Chat
 require('./modules/colors/api/colors-route')(app); // Colors
 require('./modules/countries/api/countries-routes')(app); // Countries
 require('./modules/currency/api/currency-routes')(app); // Currencies
@@ -27,22 +29,23 @@ app.get('/status', (req, res) => {
   const data = {
     uptime: process.uptime(),
     message: 'Ok',
-    date: new Date()
-  }
+    date: new Date(),
+  };
   res.status(200).send(data);
 });
 
+// Docs in JSON format
+app.get('/docs.json', (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
 // Setup Swagger API Documentation
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger.json');
-app.use(
-  '/',
-  swaggerUi.serve, 
-  swaggerUi.setup(swaggerDocument)
-);
+const swaggerUi = require('swagger-ui-express');
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(cors()); // enabling CORS for all requests
-app.use(morgan('combined'));  // adding morgan to log HTTP requests
+app.use(morgan('combined')); // adding morgan to log HTTP requests
 
 app.listen(port, () => {
   console.log(`Mock API is running on port ${port}.`);
---
+++ utils/swagger.ts
@@ -0,0 +1,92 @@
+import { Express, Request, Response } from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const { version } = require('../package.json');
+
+const options: swaggerJsdoc.Options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            version,
+            title: 'Mocked-API',
+            description:
+                'An API library for providing endpoints of mock data for use during testing. This API is still in the development and until it reaches version 1.0.0 the API is subject to change. To find out more head over to https://mocked-api.dev/',
+        },
+        host: 'api.mocked-api.dev',
+        schemes: ['https'],
+        basePath: '/',
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        tags: [
+            {
+                name: 'Animals',
+                description: 'A set of endpoints related to animal',
+            },
+            {
+                name: 'Chat',
+                description: 'A set of endpoints related to chats',
+            },
+            {
+                name: 'Colors',
+                description: 'A set of endpoints related to colors',
+            },
+            {
+                name: 'Countries',
+                description: 'A set of endpoints related to countries',
+            },
+            {
+                name: 'Currencies',
+                description: 'A set of endpoints related to currencies',
+            },
+            {
+                name: 'Emails',
+                description: 'A set of endpoints related to emails',
+            },
+            {
+                name: 'Images',
+                description: 'A set of endpoints related to obtaining placeholder image urls',
+            },
+            {
+                name: 'Music',
+                description: 'A set of endpoints related to music',
+            },
+            {
+                name: 'Names',
+                description: 'A set of endpoints to get randomly generated names',
+            },
+            {
+                name: 'Products',
+                description: 'A set of endpoints to get random product related info',
+            },
+            {
+                name: 'Quotes',
+                description: 'A set of endpoints to get random quote',
+            },
+            {
+                name: 'Socials',
+                description: 'A set of endpoints to get random social feed data',
+            },
+            {
+                name: 'Sports',
+                description: 'A set of endpoints to get random sports data',
+            },
+        ],
+    },
+    apis: ['./modules/*/api/*.ts'],
+};
+
+export const swaggerSpec = swaggerJsdoc(options);
+
+function swaggerDocs(app: Express) {
+    // Swagger page
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+    // Docs in JSON format
+    app.get('/docs.json', (req: Request, res: Response) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(swaggerSpec);
+    });
+}
+
+export default swaggerDocs;
