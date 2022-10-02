66d6abafc5b36b3dce8e595663115073177d97e3
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-10-02 20:40:36 +0700

    feat: add social instagram API

--- modules/socials/api/socials-routes.ts
+++ modules/socials/api/socials-routes.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from 'express';
 import * as core from 'express-serve-static-core';
+import { getRandomInstagramPosts } from '../utils/getRandomInstagramPosts';
 import { getRandomTwitterPosts } from '../utils/getRandomTwitterPosts';
 
-const defaultPostsCount = 10;
+const DEFAULT_POSTS_COUNT = 10;
 
-module.exports = function(app : core.Express){
+module.exports = function (app: core.Express) {
+  // Returns a set of Fake Twitter Posts
+  app.get('/socials/twitter', (req: Request, res: Response) => {
+    res.json(getRandomTwitterPosts(DEFAULT_POSTS_COUNT));
+  });
 
-    // Returns a set of Fake Twitter Posts
-    app.get("/socials/twitter", (req: Request, res: Response) => {
-        res.json(getRandomTwitterPosts(defaultPostsCount));
-    })
-
-}
+  // Returns a set of Fake Instagram Posts
+  app.get('/socials/instagram', (req: Request, res: Response) => {
+    res.json(getRandomInstagramPosts(DEFAULT_POSTS_COUNT));
+  });
+};
