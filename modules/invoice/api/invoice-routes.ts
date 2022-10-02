34461f03f3d396ec947ec8cd33a572e29fe53cb6
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-10-02 10:41:52 +0700

    add default invoice qty

--- modules/invoice/api/invoice-routes.ts
+++ modules/invoice/api/invoice-routes.ts
@@ -3,10 +3,12 @@ import * as core from 'express-serve-static-core';
 import { getQtyFromRequest } from '../../../utils/route-utils';
 import getRandomInvoices from '../utils/getRandomInvoices';
 
+const DEFAULT_INVOICE_QUANTITY = 1;
+
 module.exports = function (app: core.Express) {
   //Get a randomly generated invoice
   app.get('/invoices/:qty?', (req: Request, res: Response) => {
-    const qty = getQtyFromRequest(req, 1);
+    const qty = getQtyFromRequest(req, DEFAULT_INVOICE_QUANTITY);
     const invoices = getRandomInvoices(qty);
     res.json(invoices);
   });
