159e915637ef549f1da1f11b8277b417aed569ab
tzii <tuanvundvn@gmail.com>
2022-10-02 09:44:28 +0700

    docs: auto generate document in animal routes

--- modules/animal/api/animal-routes.ts
+++ modules/animal/api/animal-routes.ts
@@ -4,104 +4,396 @@ import { getQtyFromRequest } from '../../../utils/route-utils';
 import AnimalType from '../consts/AnimalEnum';
 import getSpeciesOfAnimal from '../utils/getSpeciesOfAnimal';
 
-module.exports = function(app: core.Express){
-
-    // Get a random list of bear species with no duplications
-    app.get("/animals/bear/species/:qty?", (req: Request, res: Response) => {
+module.exports = function (app: core.Express) {
+    /**
+     * @openapi
+     * "/animals/bear/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of bear species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/bear/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomBears = getSpeciesOfAnimal(AnimalType.Bear, qty);
-        res.json(randomBears)
-    })
+        res.json(randomBears);
+    });
 
-    // Get a random list of bird species with no duplications
-    app.get("/animals/bird/species/:qty?", (req: Request, res: Response) => {
+    /**
+     * @openapi
+     * "/animals/bird/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of bird species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/bird/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomBirds = getSpeciesOfAnimal(AnimalType.Bird, qty);
-        res.json(randomBirds)
-    })
+        res.json(randomBirds);
+    });
 
-    // Get a random list of cat species with no duplications
-    app.get("/animals/cat/species/:qty?", (req: Request, res: Response) => {
+    /**
+     * @openapi
+     * "/animals/cat/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of cat species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/cat/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomCats = getSpeciesOfAnimal(AnimalType.Cat, qty);
-        res.json(randomCats)
-    })
+        res.json(randomCats);
+    });
 
-    // Get a random list of cetecean species with no duplications
-    app.get("/animals/cetecean/species/:qty?", (req: Request, res: Response) => {
+    /**
+     * @openapi
+     * "/animals/cetecean/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of cetecean species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/cetecean/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomCetecean = getSpeciesOfAnimal(AnimalType.Cetecean, qty);
-        res.json(randomCetecean)
-    })
+        res.json(randomCetecean);
+    });
 
-     // Get a random list of cow species with no duplications
-     app.get("/animals/cow/species/:qty?", (req: Request, res: Response) => {
+    /**
+     * @openapi
+     * "/animals/cow/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of cow species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/cow/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomCow = getSpeciesOfAnimal(AnimalType.Cow, qty);
-        res.json(randomCow)
-    })
+        res.json(randomCow);
+    });
 
-     // Get a random list of crocodile species with no duplications
-     app.get("/animals/crocodile/species/:qty?", (req: Request, res: Response) => {
+    /**
+     * @openapi
+     * "/animals/crocodile/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of crocodile species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/crocodile/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomCrocodile = getSpeciesOfAnimal(AnimalType.Crocodile, qty);
-        res.json(randomCrocodile)
-    })
+        res.json(randomCrocodile);
+    });
 
-     // Get a random list of dog species with no duplications
-     app.get("/animals/dog/species/:qty?", (req: Request, res: Response) => {
+    /**
+     * @openapi
+     * "/animals/dog/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of dog species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/dog/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomDog = getSpeciesOfAnimal(AnimalType.Dog, qty);
-        res.json(randomDog)
-    })
+        res.json(randomDog);
+    });
 
-     // Get a random list of cetecean fish with no duplications
-     app.get("/animals/fish/species/:qty?", (req: Request, res: Response) => {
+    /**
+     * @openapi
+     * "/animals/fish/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of fish species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/fish/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomFish = getSpeciesOfAnimal(AnimalType.Fish, qty);
-        res.json(randomFish)
-    })
+        res.json(randomFish);
+    });
 
-     // Get a random list of horse species with no duplications
-     app.get("/animals/horse/species/:qty?", (req: Request, res: Response) => {
+    /**
+     * @openapi
+     * "/animals/horse/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of horse species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/horse/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomHorse = getSpeciesOfAnimal(AnimalType.Horse, qty);
-        res.json(randomHorse)
-    })
+        res.json(randomHorse);
+    });
 
-     // Get a random list of insect species with no duplications
-     app.get("/animals/insect/species/:qty?", (req: Request, res: Response) => {
+    /**
+     * @openapi
+     * "/animals/insect/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of insect species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/insect/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomInsect = getSpeciesOfAnimal(AnimalType.Insect, qty);
-        res.json(randomInsect)
-    })
+        res.json(randomInsect);
+    });
 
-     // Get a random list of lion species with no duplications
-     app.get("/animals/lion/species/:qty?", (req: Request, res: Response) => {
+    /**
+     * @openapi
+     * "/animals/lion/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of lion species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/lion/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomLion = getSpeciesOfAnimal(AnimalType.Lion, qty);
-        res.json(randomLion)
-    })
+        res.json(randomLion);
+    });
 
-     // Get a random list of rabbit species with no duplications
-     app.get("/animals/rabbit/species/:qty?", (req: Request, res: Response) => {
+    /**
+     * @openapi
+     * "/animals/rabbit/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of rabbit species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/rabbit/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomRabbit = getSpeciesOfAnimal(AnimalType.Rabbit, qty);
-        res.json(randomRabbit)
-    })
+        res.json(randomRabbit);
+    });
 
-     // Get a random list of rodent species with no duplications
-     app.get("/animals/rodent/species/:qty?", (req: Request, res: Response) => {
+    /**
+     * @openapi
+     * "/animals/rodent/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of rodent species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/rodent/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomRodent = getSpeciesOfAnimal(AnimalType.Rodent, qty);
-        res.json(randomRodent)
-    })
+        res.json(randomRodent);
+    });
 
-     // Get a random list of cetecean species with no duplications
-     app.get("/animals/snake/species/:qty?", (req: Request, res: Response) => {
+    /**
+     * @openapi
+     * "/animals/snake/species/{qty}":
+     *   get:
+     *     tags:
+     *     - Animals
+     *     summary: Obtain a random list of snake species with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of species you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *             example: Sun bear
+     */
+    app.get('/animals/snake/species/:qty?', (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req);
         const randomSnake = getSpeciesOfAnimal(AnimalType.Snake, qty);
-        res.json(randomSnake)
-    })
-
-}
+        res.json(randomSnake);
+    });
+};
