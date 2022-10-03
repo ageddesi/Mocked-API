c281fd144b14a247f2537be6809aa64fba45425c
siddharth <siddharth.sabron@gmail.com>
2022-10-04 00:25:04 +0530

    Adding limit to getQtyFromRequest

--- utils/route-utils.ts
+++ utils/route-utils.ts
@@ -5,23 +5,25 @@ const defaultQty = 10;
 const imageWidthDefault = 640;
 const imageHeightDefault = 480;
 
-const getQtyFromRequest = (request : Request | null, overrideDefaultQty?: number | null) : number => {
-    return request.params.qty ? parseInt(request.params.qty) : (overrideDefaultQty? overrideDefaultQty: defaultQty);
-}
+const getQtyFromRequest = (request: Request | null, overrideDefaultQty?: number | null): number => {
+  var value = parseInt(request.params.qty);
+  if (value > 100) {
+    value = 100;
+    return;
+  }
 
-const getImageDataFromRequest = (request : Request | null) : ImageDataType => {
-    const qty = request.params.qty ? parseInt(request.params.qty) : defaultQty;
-    const height = request.params.height ? parseInt(request.params.height) : imageHeightDefault;
-    const width = request.params.width ? parseInt(request.params.width) : imageWidthDefault;
-    return {
-        Width: width,
-        Height: height,
-        Qty: qty
-    }
-}
+  return value ? value : overrideDefaultQty ? overrideDefaultQty : defaultQty;
+};
 
+const getImageDataFromRequest = (request: Request | null): ImageDataType => {
+  const qty = request.params.qty ? parseInt(request.params.qty) : defaultQty;
+  const height = request.params.height ? parseInt(request.params.height) : imageHeightDefault;
+  const width = request.params.width ? parseInt(request.params.width) : imageWidthDefault;
+  return {
+    Width: width,
+    Height: height,
+    Qty: qty,
+  };
+};
 
-export {
-    getQtyFromRequest,
-    getImageDataFromRequest
-}
+export { getQtyFromRequest, getImageDataFromRequest };
