adff73c4eed47ead6be7263b57c17c08969408dd
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-10-04 16:52:32 +0700

    test: testing socials/instagram endpoint

--- modules/socials/test/socials-routes.test.ts
+++ modules/socials/test/socials-routes.test.ts
@@ -1,9 +1,11 @@
 import request from 'supertest';
 import Twitter from '../consts/Twitter';
+import Instagram from '../consts/Instagram';
+import { DEFAULT_POSTS_COUNT } from '../consts/default';
 
 const baseURL = 'http://localhost:3000';
 
-describe('twitter api endpoints', () => {
+describe('socials api endpoints', () => {
     describe('GET /socials/twitter/', () => {
         it('should returns a list of fake twitter posts', async () => {
             const response = await request(baseURL).get(`/socials/twitter/`);
@@ -17,6 +19,34 @@ describe('twitter api endpoints', () => {
             expect(twitter).toHaveProperty('comments');
             expect(twitter).toHaveProperty('retweets');
             expect(twitter).toHaveProperty('date');
+        });
+    });
+
+    describe('GET /socials/instagram/', () => {
+        it('should returns a list of fake instagram posts', async () => {
+            const response = await request(baseURL).get(`/socials/instagram/`);
+            const instagram: Instagram = response.body[0];
+
+            expect(instagram).toHaveProperty('id');
+            expect(instagram).toHaveProperty('commentCount');
+            expect(instagram).toHaveProperty('likeCount');
+            expect(instagram).toHaveProperty('imageUrl');
+            expect(instagram).toHaveProperty('createdAt');
+            expect(instagram).toHaveProperty('caption');
+            expect(instagram).toHaveProperty('width');
+            expect(instagram).toHaveProperty('height');
+            expect(instagram).toHaveProperty('accessibilityCaption');
+            expect(instagram).toHaveProperty('user.id');
+            expect(instagram).toHaveProperty('user.username');
+            expect(instagram).toHaveProperty('user.fullName');
+            expect(instagram).toHaveProperty('user.profilePicture');
+            expect(instagram).toHaveProperty('user.isVerified');
+            expect(instagram).toHaveProperty('location.name');
+            expect(instagram).toHaveProperty('location.lng');
+            expect(instagram).toHaveProperty('location.lat');
+            expect(instagram).toHaveProperty('comments');
+            expect(instagram).toHaveProperty('likes');
+            expect(response.body.length).toEqual(DEFAULT_POSTS_COUNT);
         });
     });
 });
