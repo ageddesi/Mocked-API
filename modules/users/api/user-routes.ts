5cca32beffcfc8194712ecd610a5912763f36153
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2022-10-04 21:17:22 +0200

    docs: user-routes open-api description

--- modules/users/api/user-routes.ts
+++ modules/users/api/user-routes.ts
@@ -4,9 +4,27 @@ import { getQtyFromRequest } from '../../../utils/route-utils';
 import getRandomUsers from '../utils/getRandomUsers';
 
 
-module.exports = function(app: core.Express){
+module.exports = function (app: core.Express) {
 
-    //Get a randomly generated user
+    /**
+     * @openapi
+     * "/users/{qty}":
+     *   get:
+     *     tags:
+     *     - Users
+     *     summary: Obtain a random list of users with no duplications
+     *     parameters:
+     *     - in: path
+     *       name: qty
+     *       description: The amount of users you require
+     *       type: string
+     *       default: 10
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           $ref: '#/definitions/MockUsers'
+     */
     app.get("/users/:qty?", (req: Request, res: Response) => {
         const qty = getQtyFromRequest(req, 1);
         const users = getRandomUsers(qty);
