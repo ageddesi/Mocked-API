25537e0a5a7164e79fbae00c413b001b4e6f8388
Zuhaib Shah <2016n1326@gmail.com>
2022-10-06 23:52:37 +0500

    add unit tests for the addresses endpoints

---
+++ modules/address/tests/api/address-routes.test.ts
@@ -0,0 +1,73 @@
+let request = require('supertest');
+
+const baseUrl = 'http://localhost:13000';
+
+describe('GET /address/', () => {
+	it('should return a random address', async () => {
+
+		const response = await request(baseUrl)
+			.get('/address');
+		
+		const {status, body} = response;
+		expect(status).toBe(200);
+		expect(body).toBeInstanceOf(Array);
+		expect(body.length).toBe(1);
+		expect(typeof body[0]).toBe('string');
+	})
+})
+
+describe('GET /address/:country', () => {
+	it('should return a random USA format address', async () => {
+		const response = await request(baseUrl)
+			.get('/address/usa');
+
+		const {status, body} = response;
+		expect(status).toBe(200);
+		expect(body).toBeInstanceOf(Array);
+		expect(body.length).toBe(1);
+		expect(typeof body[0]).toBe('string');
+	})
+
+	it('should return a random UK format address', async () => {
+		const response = await request(baseUrl)
+			.get('/address/uk');
+
+		const {status, body} = response;
+		expect(status).toBe(200);
+		expect(body).toBeInstanceOf(Array);
+		expect(body.length).toBe(1);
+		expect(typeof body[0]).toBe('string');
+	})
+})
+
+describe('GET /address/:country/:qty', () => {
+	it('should return given number of random USA format addresses', async () => {
+		const response = await request(baseUrl)
+			.get('/address/usa/5');
+
+		const {status, body} = response;
+		expect(status).toBe(200);
+		expect(body).toBeInstanceOf(Array);
+		expect(body.length).toBe(5);
+		expect(typeof body[0]).toBe('string');
+		expect(typeof body[1]).toBe('string');
+		expect(typeof body[2]).toBe('string');
+		expect(typeof body[3]).toBe('string');
+		expect(typeof body[4]).toBe('string');
+	})
+
+	it('should return given number of random UK format addresses', async () => {
+		const response = await request(baseUrl)
+			.get('/address/uk/5');
+
+		const {status, body} = response;
+		expect(status).toBe(200);
+		expect(body).toBeInstanceOf(Array);
+		expect(body.length).toBe(5);
+		expect(typeof body[0]).toBe('string');
+		expect(typeof body[1]).toBe('string');
+		expect(typeof body[2]).toBe('string');
+		expect(typeof body[3]).toBe('string');
+		expect(typeof body[4]).toBe('string');
+	})
+})
