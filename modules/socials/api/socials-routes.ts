79323b4dd003c16ef39292b4d64eb0a734d1bbf4
rizkyrajitha <rajithagunathilake@gmail.com>
2022-10-10 21:36:20 +0530

    added facebook post

--- modules/socials/api/socials-routes.ts
+++ modules/socials/api/socials-routes.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import * as core from 'express-serve-static-core';
 import { DEFAULT_POSTS_COUNT } from '../consts/default';
+import { getRandomFacebookPosts } from '../utils/getRandomFacebookPosts';
 import { getRandomInstagramPosts } from '../utils/getRandomInstagramPosts';
 import { getRandomTwitterPosts } from '../utils/getRandomTwitterPosts';
 
@@ -42,4 +43,23 @@ module.exports = function (app: core.Express) {
     app.get('/socials/instagram', (req: Request, res: Response) => {
         res.json(getRandomInstagramPosts(DEFAULT_POSTS_COUNT));
     });
+
+        /**
+     * @openapi
+     * '/socials/facebook':
+     *   get:
+     *     tags:
+     *     - Socials
+     *     summary: Returns a list of fake facebook posts
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: object
+     */
+         app.get('/socials/facebook', (req: Request, res: Response) => {
+            res.json(getRandomFacebookPosts(DEFAULT_POSTS_COUNT));
+        });
 };
