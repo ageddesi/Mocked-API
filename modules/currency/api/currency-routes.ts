0c571916b562f1cc0ce88974f431d731070b356f
Filip Kujawa <filip@Filips-MacBook-Pro.local>
2022-10-11 11:23:11 -0700

    interface for transaction list and open api comments

--- modules/currency/api/currency-routes.ts
+++ modules/currency/api/currency-routes.ts
@@ -40,6 +40,32 @@ module.exports = function(app: core.Express){
         res.json(addresses);
     })
 
+     /**
+     * @openapi
+     * '/currencies/digital-coins/ethereum/tx-list/:address?/:qty?':
+     *   get:
+     *     tags:
+     *     - Currency
+     *     summary: Get the list of transactions performed by an address
+     *     parameters:
+     *     - in: path
+     *       name: address
+     *       description: The address that performed the transactions
+     *       type: string
+     *     - in: path
+     *       name: qty
+     *       description: The number of transactions to be returned
+     *       type: number
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         schema:
+     *           type: json
+     *           items:
+     *             type: object
+     *             example: [{"blockNumber":"27805946","timeStamp":"9942266267","hash":"0xaN6aaOQJX5PFBhXH1RjUlygLdJKqC01qRPC0QbKZ5TIowf7iJnB5DQngiV93jik5","nonce":"18","blockHash":"0xueMI7Aje55ctOTWcCpgMKoyD6Fr5K91uGkaxbpex1gRvKrq3oPyPB5U6qMf7Ia2f","transactionIndex":"5","from":"0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC","to":"0x100807ff56cbc56f4574e515ddbefdbb8d86a7a0","value":"0.1759733580282401","gas":"2270","gasPrice":"573605567923","isError":"0","txreceipt_status":"1","input":"0xKuzGOUc0zN7sGqfFrmhLSsQKz69uZNUbHgSnnTldsCfFBHa682mvwIm2Ly3szQadWJftne0BzIO4obORd3dGKZIXAQ5XEnIveiwQ8ZXwQW9Q1MtnaSh1x5zjJ3Mc4OVHzmNxFMDe","confirmations":"33642"}]
+     */
+
     //Returns the list of transactions performed by an address
     app.get("/currencies/digital-coins/ethereum/tx-list/:address?/:qty?", (req: Request, res: Response) => {
         const address = req.params.address;
