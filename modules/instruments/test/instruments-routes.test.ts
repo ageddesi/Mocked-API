a59794691f09b792e8357a14f6cd0c63ff6b7b5f
Rodrigo <headStyleColorRed@gmail.com>
2022-10-12 17:10:03 +0200

    fixed instruments test

--- modules/instruments/test/instruments-routes.test.ts
+++ modules/instruments/test/instruments-routes.test.ts
@@ -13,7 +13,7 @@ describe('instrument api endpoints', () => {
 
     describe('GET /instruments/:filterBy?', () => {
         it('should return instrument data', async () => {
-            const filter = 'Afghanistan';
+            const filter = 'Roman tuba';
             const response = await request(app).get(`/instruments/${filter}`);
             expect(response.body.instruments[0]).toEqual(filter);
         });
