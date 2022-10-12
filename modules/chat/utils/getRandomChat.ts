818e1ca958582a088f4fd6a8fc8933b406501344
Timothée DURAND <timothee.durand70@gmail.com>
2022-10-12 21:09:08 +0200

    feat: create generateRandomMessage function

--- modules/chat/utils/getRandomChat.ts
+++ modules/chat/utils/getRandomChat.ts
@@ -1,22 +1,23 @@
-import { faker } from "@faker-js/faker";
-import getRandomUsers from "../../users/utils/getRandomUsers";
-import ChatMessage from "../consts/ChatMessage";
-import ChatRandomResponse from "../consts/ChatRandomResponse";
+import { faker } from '@faker-js/faker'
+import getRandomUsers from '../../users/utils/getRandomUsers'
+import ChatMessage from '../consts/ChatMessage'
+import ChatRandomResponse from '../consts/ChatRandomResponse'
+import { generateRandomMessage } from './generateRandomMessage'
 
-export const getRandomChat = (userCount : number, messageCount: number) : ChatRandomResponse => {
-    const users = getRandomUsers(userCount);
+export const getRandomChat = (userCount: number, messageCount: number): ChatRandomResponse => {
+  const users = getRandomUsers(userCount)
 
-    const messages : ChatMessage[] = [];
-    Array.from({length: messageCount}).forEach(() => {
-        messages.push({
-            id: faker.datatype.uuid(),
-            createdAt: faker.date.recent(faker.datatype.number({ min: 0, max: 30 })).toISOString(),
-            message: faker.lorem.sentence(),
-            userId: users[faker.datatype.number({ min: 0, max: userCount - 1 })].userId,
-        })
+  const messages: ChatMessage[] = []
+  Array.from({ length: messageCount }).forEach(() => {
+    messages.push({
+      id: faker.datatype.uuid(),
+      createdAt: faker.date.recent(faker.datatype.number({ min: 0, max: 30 })).toISOString(),
+      message: generateRandomMessage(),
+      userId: users[faker.datatype.number({ min: 0, max: userCount - 1 })].userId
     })
-    return {
-        users,
-        messages
-    }
+  })
+  return {
+    users,
+    messages
+  }
 }
