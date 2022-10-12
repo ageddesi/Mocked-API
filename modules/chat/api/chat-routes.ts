9749ed558aad6aa43b96d21eadc71f1963be8e3e
Timothée DURAND <timothee.durand70@gmail.com>
2022-10-12 21:54:47 +0200

    feat: add new chat route for various type of message

--- modules/chat/api/chat-routes.ts
+++ modules/chat/api/chat-routes.ts
@@ -12,7 +12,7 @@ module.exports = function (app: core.Express) {
      *   get:
      *     tags:
      *       - Chat
-     *     summary: Returns a random chat of 100 users and 1000 messages of various length (word, sentences, paragraphs)
+     *     summary: Returns a random chat of 100 users and 1000 sentences
      *     responses:
      *       '200':
      *         description: OK
@@ -24,12 +24,59 @@ module.exports = function (app: core.Express) {
   })
 
   /**
+   * @openapi
+   * '/chat/random/various':
+   *   get:
+   *     tags:
+   *       - Chat
+   *     summary: Returns a random chat of 100 users and 1000 messages of various length
+   *     responses:
+   *       '200':
+   *         description: OK
+   *         schema:
+   *           $ref: '#/definitions/MockChatResponse'
+   */
+  app.get('/chat/random/various/', (req: Request, res: Response) => {
+    res.json(getRandomChat(defaultUserCount, defaultMessageCount, true))
+  })
+
+  /**
+   * @openapi
+   * '/chat/random/various/{userCount}/{messageCount}':
+   *   get:
+   *     tags:
+   *       - Chat
+   *     summary: Returns a random chat of messages from defined counts using randomly generated sentences
+   *     parameters:
+   *       - in: path
+   *         name: userCount
+   *         description: The amount of users the messages can be generated from
+   *         type: string
+   *         default: 100
+   *       - in: path
+   *         name: messageCount
+   *         description: The amount of messages that will be returned
+   *         type: string
+   *         default: 1000
+   *     responses:
+   *       '200':
+   *         description: OK
+   *         schema:
+   *           $ref: '#/definitions/MockChatResponse'
+   */
+  app.get('/chat/random/various/:userCount/:messageCount', (req: Request, res: Response) => {
+    const userCount = req.params.userCount !== '' ? parseInt(req.params.userCount) : defaultUserCount
+    const messageCount = req.params.messageCount !== '' ? parseInt(req.params.messageCount) : defaultMessageCount
+    res.json(getRandomChat(userCount, messageCount, true))
+  })
+
+  /**
      * @openapi
      * '/chat/random/{userCount}/{messageCount}':
      *   get:
      *     tags:
      *       - Chat
-     *     summary: Returns a random chat of messages from defined counts using randomly generated messages of various length (word, sentences, paragraphs)
+     *     summary: Returns a random chat of messages from defined counts using randomly generated sentences
      *     parameters:
      *       - in: path
      *         name: userCount
