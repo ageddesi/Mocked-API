9302250e743ecce1b4df9d8eddab112ca8daf5cf
lauk20 <klau6899@gmail.com>
2022-10-13 21:52:07 -0400

    Change route to /video instead of /video/random

--- modules/video/api/video-routes.ts
+++ modules/video/api/video-routes.ts
@@ -3,7 +3,7 @@ import * as core from 'express-serve-static-core';
 import getVideo from '../utils/getVideo';
 
 module.exports = function (app: core.Express) {
-    app.get('/video/random', (req: Request, res: Response) => {
+    app.get('/video', (req: Request, res: Response) => {
         res.json(getVideo());
     })
 }
