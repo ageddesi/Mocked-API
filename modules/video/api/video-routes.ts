fe7b38ab1e84c5e6c092ebcbcfd7a232f8ea2650
lauk20 <klau6899@gmail.com>
2022-10-13 20:18:48 -0400

    Add /video/random route

---
+++ modules/video/api/video-routes.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+import * as core from 'express-serve-static-core';
+import getVideo from '../utils/getVideo';
+
+module.exports = function (app: core.Express) {
+    app.get('/video/random', (req: Request, res: Response) => {
+        res.json(getVideo());
+    })
+}
