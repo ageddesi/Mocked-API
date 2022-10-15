0184bae59488ec90e5ec7c22e1a905ed2a8ac2d1
lauk20 <klau6899@gmail.com>
2022-10-15 16:03:37 -0400

    Add OpenAPI definition MockVideo

--- modules/video/consts/Video.ts
+++ modules/video/consts/Video.ts
@@ -1,3 +1,29 @@
+
+/**
+ * @openapi
+ * definitions:
+ *   MockVideo:
+ *     type: object:
+ *     properties:
+*        title:
+*          type: string
+*        description:
+*          type: string
+*        date:
+*          type: string
+*        author:
+*          type: string
+*        views:
+*          type: number
+*        likes:
+*          type: number
+*        dislikes:
+*          type: number
+*        runtimeSeconds:
+*          type: number
+*        maxQuality:
+*          type: string
+ */
 type Video = {
     title: string,
     description: string,
