d2c6cc9e6eebdb5c71148c69462aa95577212a1c
lauk20 <klau6899@gmail.com>
2022-10-15 19:39:45 -0400

    Add test for /video/:qty endpoint

--- modules/video/tests/api/video-routes.test.ts
+++ modules/video/tests/api/video-routes.test.ts
@@ -2,7 +2,7 @@ import app from "../../../../app";
 import resolutions from "../../consts/videoResolutions"
 let request = require('supertest');
 
-// video-routes.ts tests
+// /video/ test
 describe('GET /video/', () => {
     it('should return random video data', async () => {
         const response = await request(app).get('/video/');
@@ -17,5 +17,16 @@ describe('GET /video/', () => {
         expect(body.likes).toBeLessThanOrEqual(body.views);
         expect(body.dislikes).toBeLessThanOrEqual(body.views - body.likes);
         expect(resolutions).toContain(body.maxQuality);
+    })
+})
+
+// /video/:qty/ test
+describe('GET /video/:qty', () => {
+    it('should return array of length specified of random video data', async () => {
+        const response = await request(app).get('/video/10');
+        const {status, body} = response;
+        expect(status).toBe(200);
+        expect(body).toBeDefined();
+        expect(body.length).toBe(10);
     })
 })
