02013f03fbf7aa96c97756c0433412d1bd03feaf
Christopher Vestman <christophervestman@gmail.com>
2022-10-16 23:50:52 +0200

    feat: add serie A endpoint

---
+++ modules/sports/data/football-serie-a-league-2022.ts
@@ -0,0 +1,124 @@
+const SerieALeagueData2022 = [
+    {
+        teamName: 'Atalanta',
+        location: 'Bergamo',
+        stadium: 'Gewiss Stadium',
+        capacity: '21,300',
+    },
+    {
+        teamName: 'Bologna',
+        location: 'Bologna',
+        stadium: "Stadio Renato Dall'Ara",
+        capacity: '38,279',
+    },
+    {
+        teamName: 'Cremonese',
+        location: 'Cremona',
+        stadium: 'Stadio Giovanni Zini',
+        capacity: '20,641',
+    },
+    {
+        teamName: 'Empoli',
+        location: 'Empoli',
+        stadium: 'Stadio Carlo Castellani',
+        capacity: '16,800',
+    },
+    {
+        teamName: 'Fiorentina',
+        location: 'Florence',
+        stadium: 'Stadio Artemio Franchi',
+        capacity: '43,147',
+    },
+    {
+        teamName: 'Hellas Verona',
+        location: 'Verona',
+        stadium: "Stadio Marc'Antonio Bentegodi",
+        capacity: '39,211',
+    },
+    {
+        teamName: 'Inter',
+        location: 'Milano',
+        stadium: 'Stadio Giuseppe Meazza',
+        capacity: '80,018',
+    },
+    {
+        teamName: 'Juventus',
+        location: 'Turin',
+        stadium: 'Allianz Stadium',
+        capacity: '41,507',
+    },
+    {
+        teamName: 'Lazio',
+        location: 'Rome',
+        stadium: 'Stadio Olimpico',
+        capacity: '72,698',
+    },
+    {
+        teamName: 'Lecce',
+        location: 'Lecce',
+        stadium: 'Stadio Via del Mare',
+        capacity: '40,670',
+    },
+    {
+        teamName: 'Milan',
+        location: 'Milano',
+        stadium: 'San Siro',
+        capacity: '80,018',
+    },
+    {
+        teamName: 'Monza',
+        location: 'Monza',
+        stadium: 'U-Power Stadium',
+        capacity: '15,039',
+    },
+    {
+        teamName: 'Napoli',
+        location: 'Naples',
+        stadium: 'Stadio Diego Armando Maradona',
+        capacity: '50,726',
+    },
+    {
+        teamName: 'Roma',
+        location: 'Rome',
+        stadium: 'Stadio Olimpico',
+        capacity: '72,698',
+    },
+    {
+        teamName: 'Salernitana',
+        location: 'Salerno',
+        stadium: 'Stadio Arechi',
+        capacity: '37,800',
+    },
+    {
+        teamName: 'Sampdoria',
+        location: 'Genoa',
+        stadium: 'Stadio Luigi Ferraris',
+        capacity: '36,599',
+    },
+    {
+        teamName: 'Sassuolo',
+        location: 'Sassuolo',
+        stadium: 'Mapei Stadium',
+        capacity: '21,525',
+    },
+    {
+        teamName: 'Spezia',
+        location: 'La Spezia',
+        stadium: 'Stadio Alberto Picco',
+        capacity: '11,466',
+    },
+    {
+        teamName: 'Torino',
+        location: 'Turin',
+        stadium: 'Stadio Olimpico Grande Torino',
+        capacity: '28,177',
+    },
+    {
+        teamName: 'Udinese',
+        location: 'Udine',
+        stadium: 'Dacia Arena',
+        capacity: '25,144',
+    },
+];
+
+export default SerieALeagueData2022;
