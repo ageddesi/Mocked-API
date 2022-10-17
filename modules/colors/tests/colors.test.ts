f0f0869580be954f0d84780b2dbd4ed8a3abf4e1
Mike Hingley <computa_mike@hotmail.com>
2022-10-17 22:48:09 +0100

    Update tests

--- modules/colors/tests/colors.test.ts
+++ modules/colors/tests/colors.test.ts
@@ -1,29 +1,28 @@
 import request from 'supertest';
-const baseURL = 'http://localhost:3000';
-
+import app from "../../../app" // "../../../../app";
 describe('color api endpoints', () => {
  
   describe('GET /color/', () => {
     it('requesting with no qty returns a list of 1 color', async () => {
       const qty = 1;
-      const response = await request(baseURL).get(`/colors/`);
+      const response = await request(app).get(`/colors/`);
       expect(response.body.length).toBe(qty);
     });
     it('requesting with non-numerical qty returns a list of 100 color', async () => {
       // I think this is potentially a bug - we probably should be retuning 1 colour, or throwing an error.
       const qty = 100;
-      debugger;
-      const response = await request(baseURL).get(`/colors/foo`);
+      const response = await request(app).get(`/colors/foo`);
+      
       expect(response.body.length).toBe(qty);
     });
     it('requesting with quantity of 2 should return a list of 2 colors', async () => {
       const qty = 2;
-      const response = await request(baseURL).get(`/colors/${qty}`);
+      const response = await request(app).get(`/colors/${qty}`);
       expect(response.body.length).toBe(qty);
     });
     it('requesting with quantity of 200 should return a list of 100 colors', async () => {
       const qty = 200;
-      const response = await request(baseURL).get(`/colors/${qty}`);
+      const response = await request(app).get(`/colors/${qty}`);
       expect(response.body.length).toBe(100);
     });
 
