9def43a0b1c9bfa62240cf7f16251b173c5ad2ab
Filip Kujawa <filip@Filips-MacBook-Pro.local>
2022-10-19 12:51:31 -0700

    add route tests

--- modules/currency/utils/GetDigitalCurrencyBalance.ts
+++ modules/currency/utils/GetDigitalCurrencyBalance.ts
@@ -3,7 +3,7 @@ import digital_currency_units from '../data/digital-currency-units';
 
 const getDigitalCurrencyBalance = (network: string, address: string) : addressBalance => {
 
-    const unit = digital_currency_units[address];
+    const unit = digital_currency_units[network];
     const amount: number = Number((Math.random() * (1000 - 0.001) + 0.001).toFixed(3));
 
     const addressBalance = {
