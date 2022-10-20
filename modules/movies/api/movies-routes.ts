03ce104f9deb54472e4682a4b406b43d7b042b84
Fernando Guerrero <devrrior@gmail.com>
2022-10-20 00:14:37 -0500

    docs(movies/api/movies-routes.ts): add OpenApi docs for both endpoints

--- modules/movies/api/movies-routes.ts
+++ modules/movies/api/movies-routes.ts
@@ -3,6 +3,27 @@ import getMovieById from '../utils/getMovieById';
 import getRandomMovies from '../utils/getRandomMovies';
 
 module.exports = function (app: core.Express) {
+    /**
+     * @openapi
+     * '/api/movies/':
+     *  get:
+     *    tags:
+     *    - Movies
+     *    summary: Returns a random array of ids
+     *    parameters:
+     *    - in: query
+     *      name: qty
+     *      schema:
+     *          type: number
+     *          default: 10
+     *    responses:
+     *      '200':
+     *        description: OK
+     *        schema:
+     *          type: array
+     *          items:
+     *            $ref: '#/definitions/MockMovie'
+     */
     app.get('/movies', (req, res) => {
         const { qty = 10 } = req.query;
         const movies = getRandomMovies(Number(qty));
@@ -10,6 +31,32 @@ module.exports = function (app: core.Express) {
         return res.json(movies);
     });
 
+    /**
+     * @openapi
+     * '/movies/{id}':
+     *  get:
+     *    tags:
+     *    - Movies
+     *    summary: Get movie by id
+     *    parameters:
+     *    - in: path
+     *      name: id
+     *      type: number
+     *      required: true
+     *    responses:
+     *      '200':
+     *        description: OK
+     *        schema:
+     *          $ref: '#/definitions/MockMovie'
+     *      '404':
+     *        description: Not Found
+     *        schema:
+     *          type: object
+     *          properties:
+     *            message:
+     *              type: string
+     *              example: Movie not found
+     */
     app.get('/movies/:id', (req, res) => {
         const { id } = req.params;
         const movie = getMovieById(Number(id));
